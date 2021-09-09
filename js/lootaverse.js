db5f46e845246ea44257143cabe1a752c81821f5
Yotam Bar-on <tudmotu@gmail.com>
2021-09-09 16:56:09 +0300

    Add collections for Stats and Inventory slots

--- js/lootaverse.js
+++ js/lootaverse.js
@@ -15,6 +15,7 @@ class EditModal {
     constructor (elementId, conn) {
         this.element = document.getElementById(elementId);
         this.component = new bootstrap.Modal(this.element);
+        this.title = this.element.querySelector('.modal-title');
         this.dropdown = this.element.querySelector('.dropdown');
         this.dropdownButton = this.dropdown.querySelector('[data-bs-toggle]');
         this.menu = this.dropdown.querySelector('.dropdown-menu');
@@ -22,6 +23,10 @@ class EditModal {
         this.img = this.element.querySelector('#token-image');
 
         this.selectedCollection = null;
+
+        this.element.addEventListener('hidden.bs.modal', () => {
+            this.reset();
+        });
 
         this.menu.addEventListener('click', event => {
             const selected = event.target.closest('[data-collection-contract]');
@@ -50,6 +55,14 @@ class EditModal {
         this.component.show();
     }
 
+    reset () {
+        this.title.textContent = '';
+        this.dropdownButton.textContent = 'Choose collection...';
+        this.menu.innerHTML = '';
+        this.input.value = '';
+        this.img.src = '';
+    }
+
     populateCollections (collections) {
         this.menu.innerHTML = `
             ${collections.map(collection => `
@@ -60,6 +73,10 @@ class EditModal {
                 </li>
             `).join('')}
         `;
+    }
+
+    setTitle (title) {
+        this.title.textContent = title;
     }
 
     sanitizeURI (uri) {
@@ -87,16 +104,31 @@ async function init () {
             { name: 'Forgotten Runes Wizard\'s Cult', contract: '0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42' },
             { name: 'MetaHero Universe', contract: '0x6dc6001535e15b9def7b0f6a20a2111dfa9454e2' },
         ]);
+
+        modal.setTitle('Choose your avatar');
+
         modal.open();
     });
 
     statsSlotButton.onClick(() => {
-        console.log('stats clicked');
+        modal.populateCollections([
+            { name: 'Characters (for Adventurers)', contract: '0x7403ac30de7309a0bf019cda8eec034a5507cbb3' },
+            { name: 'Role for Metaverse', contract: '0xcd4d337554862f9bc9ffffb67465b7d643e4e3ad' },
+        ]);
+
+        modal.setTitle('Choose your stats');
+
         modal.open();
     });
 
     inventorySlotButton.onClick(() => {
-        console.log('inventory clicked');
+        modal.populateCollections([
+            { name: 'Loot', contract: '0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7' },
+            { name: 'xLoot', contract: '0x8bf2f876e2dcd2cae9c3d272f325776c82da366d' }
+        ]);
+
+        modal.setTitle('Choose your inventory');
+
         modal.open();
     });
 }
