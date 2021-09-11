2c91054a036dfff6d3df87530775d90a92a16c72
Yotam Bar-on <tudmotu@gmail.com>
2021-09-11 10:36:27 +0300

    Use Web3 wallet for provider if exists

--- composite-loot/index.js
+++ composite-loot/index.js
@@ -2,8 +2,16 @@ import { ethers } from "../node_modules/ethers/dist/ethers.esm.js";
 import { SlotButton } from './SlotButton.js';
 import { EditModal } from './EditModal.js';
 
-const RPC_ENDPOINT = 'https://cloudflare-eth.com/';
-const provider = new ethers.providers.JsonRpcProvider(RPC_ENDPOINT);
+let provider;
+
+if (window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+}
+else {
+    console.log('No wallet present, connecting to Ethereum via Cloudflare');
+    const RPC_ENDPOINT = 'https://cloudflare-eth.com/';
+    provider = new ethers.providers.JsonRpcProvider(RPC_ENDPOINT);
+}
 
 const avatarSlotButton = new SlotButton('avatar');
 const statsSlotButton = new SlotButton('stats');
