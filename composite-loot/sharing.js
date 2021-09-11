b85b30d84c0dc194e39b5e32e6927cdb49baf6fb
Yotam Bar-on <tudmotu@gmail.com>
2021-09-11 19:52:23 +0300

    Fix Composite Loot page layout + add background color to copied images

--- composite-loot/sharing.js
+++ composite-loot/sharing.js
@@ -2,12 +2,13 @@ import '../node_modules/html-to-image/dist/html-to-image.js';
 
 const copyBtn = document.getElementById('copy-composite');
 
-console.log(htmlToImage);
-
 copyBtn.addEventListener('click', async () => {
     const builder = document.getElementById('composite-loot-container');
+    const backgroundColor = getComputedStyle(document.body).backgroundColor;
 
-    const blobData = await htmlToImage.toBlob(builder);
+    const blobData = await htmlToImage.toBlob(builder, {
+        backgroundColor
+    });
 
     await navigator.clipboard.write([
         new ClipboardItem({
